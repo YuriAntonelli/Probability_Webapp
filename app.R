b6b13210ad851f32f1c23099a8dc3a1ed3b8fa65
YuriAntonelli <yuriantonelli1999@gmail.com>
2024-09-03 23:59:05 +0200

    first working prototype

--- app.R
+++ app.R
@@ -1,33 +1,46 @@
 library(shiny)
 
-# Define UI for the application
+# Define the UI
 ui <- fluidPage(
+  tags$head(
+    tags$script(src = "https://cdn.jsdelivr.net/npm/mathjax@2.7.9/MathJax.js?config=TeX-AMS-MML_HTMLorMML")
+  ),
   
-  # Application title
-  titlePanel("Probability Problems"),
+  titlePanel("Select a Problem to View"),
   
-  # Main content
-  mainPanel(
-    h3("Select a file to view:"),
-    uiOutput("file_links")
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(
+        inputId = "problem", 
+        label = "Choose a problem:", 
+        choices = c("Monty Hall Problem" = "problems_repository/monty_hall.html",
+                    "Birthday Problem" = "problems_repository/birthday_problem.html",
+                    "Newton Pepys Problem" = "problems_repository/newton_pepys_problem.html",
+                    "St Petersburg" = "problems_repository/st_petersburg.html",
+                    "The Montmort Problem" = "problems_repository/the_montmort_problem.html")
+      )
+    ),
+    
+    mainPanel(
+      # Div to display the selected HTML content
+      uiOutput("markdown")
+    )
   )
 )
 
-#-------------------------------------------------------------------------------
-
-# Define server 
+# Define the server logic
 server <- function(input, output, session) {
-  
-  # Generate clickable list of files
-  output$file_links <- renderUI({
-    tags$ul(
-      tags$li(a(href = "/problems_repository/birthday_problem.html", "Birthday Problem", target = "_blank")),
+  output$markdown <- renderUI({
+    # Read the HTML content
+    content <- includeHTML(input$problem)
+    
+    # Return the content wrapped in a div and trigger MathJax after rendering
+    tagList(
+      tags$div(HTML(content)),
+      tags$script("MathJax.Hub.Queue(['Typeset', MathJax.Hub]);")
     )
   })
 }
 
-#-------------------------------------------------------------------------------
-
 # Run the application
 shinyApp(ui = ui, server = server)
-
